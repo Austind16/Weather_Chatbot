e9062fa895d0fec604ed4f806c2fb417a94320f3
Austin Dsouza <austu16@gmail.com>
2026-07-09 18:25:10 +0530

    Too many redirects fix

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def index():
                 db.session.commit()
 
 
-    return redirect(url_for("index"))
+        return redirect(url_for("index"))
 
     return render_template(
         "index.html",
